2d0717df132ffa4fbcc74ca7fbd234bc93fbfe8d
Emrul-Hasan <imrulhasan003@gmail.com>
2022-09-10 01:28:04 +0600

    Basic SuperGlobals

---
+++ Basic PHP/05_SuperGlobal.php
@@ -0,0 +1,42 @@
+<?php
+$x = 10;
+$y = 20;
+function addtion(){
+    $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y'];
+
+}
+addtion();
+echo $z;
+echo "<br>";
+
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER['HTTP_REFERER'];
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
+echo "<br>";
+?>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  Name: <input type="text" name="fname">
+  <input type="submit">
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // collect value of input field
+  $name = $_REQUEST['fname'];
+  if (empty($name)) {
+    echo "Name is empty";
+  } else {
+    echo $name;
+  }
+}
+
+
+?>
